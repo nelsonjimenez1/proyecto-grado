6d7fdcb325b43339a72d79367adbd4ddceb96424
AntonioRiveraDelgado <gustavorivera@javeriana.edu.co>
2021-04-12 17:34:27 -0500

    docker generator

--- src/main/java/com/javeriana/edu/co/DockerGenerator.java
+++ src/main/java/com/javeriana/edu/co/DockerGenerator.java
@@ -1,6 +1,7 @@
 package com.javeriana.edu.co;
 
-import static com.javeriana.edu.co.CreateProjectMicroWeb.fileSeparator;
+import static com.javeriana.edu.co.CreateProjectMicroServices.fileSeparator;
+import com.javeriana.edu.co.Utils.XMLUtils;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,6 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -26,16 +30,18 @@ import java.util.logging.Logger;
 public class DockerGenerator {
     public static int lineJump = 4; 
     public static String image ="openjdk:8-jre";
+    
+    public XMLUtils xmlU;
     DockerGenerator(){
-
+        xmlU = new XMLUtils();
     }
     public void generateDockerFile(String microName){
-        String[] split = {"output", microName, "DockerFile"};
+        String[] split = {"output", microName, "Dockerfile"};
         List<String> lines = new ArrayList<>(); 
         String pathGeneric = String.join(fileSeparator, split);
         File file = new File(pathGeneric); 
         String from = "FROM " + image; 
-        String add = "ADD " + "target"+ fileSeparator + microName+"-" +getADDtext(microName) + " "+ "app.jar"; 
+        String add = "ADD " + "target"+ "/"  +getADDText(microName) + " "+ "app.jar"; 
         String expose = "EXPOSE " + getPort(microName); 
         String coment ="# Optional default command\n" + "# ENTRYPOINT [\"java\",\"-jar\",\"/app.jar\",\"reg\"]";
         lines.add(from);
@@ -60,49 +66,16 @@ public class DockerGenerator {
             Logger.getLogger(DockerGenerator.class.getName()).log(Level.SEVERE, null, e);
         }
     }
-    public String getADDtext(String microName){
-        String textAdd =""; 
-        String version = "";
-        String packaging = ""; 
-        String[] splitPom = {"output", microName, "pom.xml"};
-        String pathGenericPom = String.join(fileSeparator, splitPom);
-        BufferedReader br;
-        File fPom = new File(pathGenericPom);
-        FileReader fr;
-        try {
-            fr = new FileReader(fPom);
-            if(fPom.exists()){
-            br = new BufferedReader(fr);
-            String line; 
-            String [] textElements;
-            String text;
-            int cont = 0; 
-            do {
-                line = br.readLine();
-                if(line != null && line.length() > 0){
-                cont = 0; 
-                textElements = line.split("<");
-                    if(textElements.length > 1){
-                        text = textElements[1].split(">")[0];
-                        if(text.equals("version")){
-                            version = textElements[1].split(">")[1];; 
-                        }
-                        if(text.equals("packaging")){
-                            packaging = textElements[1].split(">")[1];; 
-                        }
-                    }
-                }else{
-                    cont ++; 
-                }
-            }while(cont < lineJump && (version.equals("") || packaging.equals("")) );   
-            textAdd = version +"." + packaging;
-        }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(DockerGenerator.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(DockerGenerator.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return textAdd; 
+    public String getADDText(String microName){
+        Document d = xmlU.openXMLFile(System.getProperty("user.dir") +fileSeparator+"output"+fileSeparator + microName + fileSeparator+ "pom.xml");
+        ArrayList<Node> nodoVersion = xmlU.readXMLNodes(d, "/project/version");
+        ArrayList<Node> nodoArtifactID = xmlU.readXMLNodes(d, "/project/artifactId");
+        ArrayList<Node> nodoPackaging = xmlU.readXMLNodes(d,"/project/packaging");
+        String version = nodoVersion.get(0).getChildNodes().item(0).getNodeValue(); 
+        String artifactId = nodoArtifactID.get(0).getChildNodes().item(0).getNodeValue();
+        String packaging = nodoPackaging.get(0).getChildNodes().item(0).getNodeValue();
+        String add = artifactId +"-"+version+"."+packaging;
+        return add;
     }
     public String getPort(String microName){
         String port ="";  
@@ -141,4 +114,5 @@ public class DockerGenerator {
         }
         return port; 
     }
+
 }
