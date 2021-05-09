e861c51de1549f3a10032556ea47898cf782dcbe
nelson jimenez <jimeneznelsondavid1@gmail.com>
2021-05-09 11:00:11 -0500

    terminaron pruebas unitarias

--- src/test/java/com/javeriana/edu/co/Graph/GraphTest.java
+++ src/test/java/com/javeriana/edu/co/Graph/GraphTest.java
@@ -28,104 +28,105 @@ public class GraphTest {
     @Test
     @DisplayName("")
     public void testGetEdgesSameMicroserviceBySrcNodeId() {
-        boolean sw = false;
         ArrayList<Edge> edgesSameMicroserviceBySrcNodeId = graph.getEdgesSameMicroserviceBySrcNodeId("owner.org.springframework.samples.petclinic.owner.OwnerController", "owner");
-        if (edgesSameMicroserviceBySrcNodeId.size() == 9) {
-            sw = true;
-        }
-        
-        assertEquals(true, sw);
+        assertEquals(9, edgesSameMicroserviceBySrcNodeId.size());
     }
    
     @Test
     @DisplayName("")
-    public void testGetNodeMethodsBySrcNodeId() {
-        boolean sw = false;
-        ArrayList<Vertex> nodeMethodsBySrcNodeId = graph.getNodeMethodsBySrcNodeId("owner.org.springframework.samples.petclinic.owner.OwnerRepository");
-        System.out.println("ayuda" + nodeMethodsBySrcNodeId.size());
-    }
-
-    @Test
-    @DisplayName("")
     public void testGetNodeElementsSameMicroserviceBySrcNodeId() {
-
+        ArrayList<Vertex> nodeElementsSameMicroserviceBySrcNodeId = graph.getNodeElementsSameMicroserviceBySrcNodeId("Has Method", "vet.org.springframework.samples.petclinic.vet.VetRepository", "vet");
+        assertEquals(1, nodeElementsSameMicroserviceBySrcNodeId.size());
     }
     
     @Test
     @DisplayName("")
     public void testGetNodesByMicroservice() {
-
+        ArrayList<Vertex> nodeByNodeId = graph.getNodesByMicroservice("vet");
+        assertEquals(13, nodeByNodeId.size());
     }
     
     @Test
     @DisplayName("")
     public void testGetEdgesByDstNodeId() {
-        
+        ArrayList<Edge> edgesByDstNodeId = graph.getEdgesByDstNodeId("owner.org.springframework.samples.petclinic.owner.OwnerRepository.findById(Integer)");
+        assertEquals(2, edgesByDstNodeId.size());
     }
    
     @Test
     @DisplayName("")
     public void testGetListMicroservices() {
-      
+        ArrayList<String> listMicroservices = graph.getListMicroservices();
+        assertEquals(5, listMicroservices.size());
     }
 
     @Test
     @DisplayName("")
     public void testGetControllers() {
-       
+        HashMap<String, ArrayList<Vertex>> controllers = graph.getControllers();
+        assertEquals(5, controllers.size());
     }
 
     @Test
     @DisplayName("")
     public void testGetEntitiesProjectWeb() {
-
+        HashMap<String, Vertex> entitiesProjectWeb = graph.getEntitiesProjectWeb();
+        assertEquals(10, entitiesProjectWeb.size());
     }
    
     @Test
     @DisplayName("")
     public void testGetEntitiesByMicroservice() {
-     
+        Set<String> entitiesByMicroservice = graph.getEntitiesByMicroservice("vet");
+        assertEquals(6, entitiesByMicroservice.size());
     }
 
     @Test
     @DisplayName("")
     public void testGetMethodsByClassId() {
-
+        ArrayList<Vertex> methodsByClassId = graph.getMethodsByClassId("owner.org.springframework.samples.petclinic.owner.OwnerController");
+        assertEquals(5, methodsByClassId.size());
     }
 
     @Test
     @DisplayName("")
     public void testGetMethodsByMethodId() {
-
+        ArrayList<Vertex> methodsByMethodId = graph.getMethodsByMethodId("pet1.org.springframework.samples.petclinic.owner.PetController.update(int, int, Pet)");
+        assertEquals(1, methodsByMethodId.size());
     }
    
     @Test
     @DisplayName("")
     public void testGetMethodsDistinctMicroservices() {
-
+        ArrayList<Vertex> methodsDistinctMicroservices = graph.getMethodsDistinctMicroservices("pet1.org.springframework.samples.petclinic.owner.PetController.update(int, int, Pet)");
+        assertEquals(1, methodsDistinctMicroservices.size());
     }
 
     @Test
     @DisplayName("")
     public void testGetMainByMicroservice() {
-
+        Vertex mainByMicroservice = graph.getMainByMicroservice("pet1");
+        assertEquals("PetClinicApplication", mainByMicroservice.getName());
     }
     
     @Test
     @DisplayName("")
     public void testGetParentByMethodId() {
- 
+        Vertex parentByMethodId = graph.getParentByMethodId("visit.org.springframework.samples.petclinic.owner.VisitController.create(Visit, int, int)");
+        assertEquals("VisitController", parentByMethodId.getName());
     }
  
     @Test
     @DisplayName("")
     public void testGetFieldsByClassId() {
- 
+        ArrayList<Vertex> fieldsByClassId = graph.getFieldsByClassId("pet1.org.springframework.samples.petclinic.owner.PetController");
+        assertEquals(2, fieldsByClassId.size());
     }
   
     @Test
     @DisplayName("")
     public void testNeedExpose() {
-
+        boolean needExpose = graph.needExpose("owner.org.springframework.samples.petclinic.owner.OwnerRepository");
+        assertEquals(true, needExpose);
     }
 }
